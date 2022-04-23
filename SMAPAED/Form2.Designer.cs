bd17d512e46eb2a1a86e226da55acb4165f4a2a3
NoeVelasquez <noemirosiovera305@gmail.com>
2022-04-23 09:20:31 -0400

    Agregado agente en form2

--- SMAPAED/Form2.Designer.cs
+++ SMAPAED/Form2.Designer.cs
@@ -28,12 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.ControlAgEVA = new AxDoubleAgentCtl.AxDaControl();
+            ((System.ComponentModel.ISupportInitialize)(this.ControlAgEVA)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ControlAgEVA
+            // 
+            this.ControlAgEVA.Enabled = true;
+            this.ControlAgEVA.Location = new System.Drawing.Point(539, 186);
+            this.ControlAgEVA.Name = "ControlAgEVA";
+            this.ControlAgEVA.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("ControlAgEVA.OcxState")));
+            this.ControlAgEVA.Size = new System.Drawing.Size(32, 32);
+            this.ControlAgEVA.TabIndex = 0;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ControlAgEVA);
+            this.Name = "Form2";
             this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ControlAgEVA)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private AxDoubleAgentCtl.AxDaControl ControlAgEVA;
     }
 }
--- SMAPAED/Form2.cs
+++ SMAPAED/Form2.cs
@@ -7,14 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AgentObjects;
+using DoubleAgentCtl;
 
 namespace SMAPAED
 {
     public partial class Form2 : Form
     {
+        private AgentObjects.IAgentCtlCharacter Avatar;
         public Form2()
         {
             InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            ControlAgEVA.Characters.Load("Robby", "Robby.acs");
+            Avatar = (IAgentCtlCharacter)ControlAgEVA.Characters["Robby"];
+            Avatar.Show(0);
+            Avatar.MoveTo(565, 284, 1);
+            Avatar.Speak("hola Bienvenido al sistema de apoyo para Matematicas");
         }
     }
 }
